46f3bead7584bb99cd1037514e631adf0a7b4581
ayadss <44919668+ayadss@users.noreply.github.com>
2018-11-28 17:18:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "!";
 var adminprefix = '!'
 
-const developers = ["441584713799303183","id"]
+const developers = ["511902311300202498","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;

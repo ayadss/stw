dd27064360cc757cb257ebac87a82527cc7a5d4d
ayadss <44919668+ayadss@users.noreply.github.com>
2018-11-28 17:22:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+const prefix ="!";
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+ console.log("هلا التوت شغل تمم "); 
+console.log("log");
+});
+
+
+
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "!";
